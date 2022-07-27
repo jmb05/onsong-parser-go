30f2eea5c345bae3237de7cddf32fa6b5614ad08
jmb05 <jmb05@web.de>
2022-07-28 00:29:14 +0200

    Added more error handling

--- html/html.go
+++ html/html.go
@@ -2,21 +2,29 @@ package html
 
 import (
 	"bytes"
-	"log"
+	"fmt"
 	"text/template"
 
 	"github.com/jmb05/Onsong-Parser-go/onsong"
+	"github.com/jmb05/styling"
 )
 
 func CreateHtml(song onsong.Song, templatePath string) string {
 	tmpl, err := template.ParseFiles(templatePath)
 	if err != nil {
-		log.Panic(err)
+		parseError(err)
+		return ""
 	}
 	var buffer bytes.Buffer
 	err = tmpl.Execute(&buffer, &song)
 	if err != nil {
-		log.Panic(err)
+		parseError(err)
+		return ""
 	}
 	return buffer.String()
+}
+
+func parseError(err error) {
+	fmt.Println(styling.Color("red", "Error applying template"))
+	fmt.Println(err)
 }
